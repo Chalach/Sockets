12ff6f0c7d664a4006c8ffd314173a61d9993c14
Chalach <redbull.al8@gmail.com>
2017-04-04 04:11:16 -0700

    Server

--- Client/Client.c
+++ Client/Client.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(void){
     printf("Client");
--- Server/Server.c
+++ Server/Server.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int createSocket();
+void createConnection(int socket);
 
 int main(){
-    printf("Server\n");
-    for (int i = 1; i <= 10; ++i) {
-        printf("%i\n", i);
-    }
+    createConnection(createSocket());
+
     return 0;
+}
+
+int createSocket(){
+    int sSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (sSocket == -1){
+        perror("Socket wurde nicht erstellt!");
+        return 1;
+    }
+    return sSocket;
+}
+
+void createConnection(int socket){
+    if (connect(socket, (sockaddr *) & serv_addr, sizeof(sockaddr)) == -1) {
+        perror ("connect()");
+    }
 }
 
